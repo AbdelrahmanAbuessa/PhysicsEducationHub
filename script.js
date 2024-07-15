0527e67595be467b5add817b771c2796f6b23ab4
Abdelrahman Ibrahim <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2024-07-15 09:28:08 +0300

    Adding Forces and Vectors #1

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 let menu = document.querySelector(".layover");
+let canvas = document.querySelector(".axisgrid");
+
+let forceMagnitude = document.querySelector("#mag"); 
+let forceDirection = document.querySelector("#dir"); 
 
 document.addEventListener("click", function (e) {
     let elementTarget = e.target;
-    if (elementTarget.hasAttribute("btn")) {
-        if (elementTarget.id === "activateMenu") {
-            menu.display = "flex";
+    if (elementTarget.id === "activateMenu") {
+        menu.style.setProperty("display", "flex");
+    } else if (elementTarget.id === "disableMenu") {
+        menu.style.setProperty("display", "none");
+    } else if (elementTarget.id === "addForce") {
+        if (forceDirection.value === "" || forceMagnitude.value === "") {
+            alert("Please insert a number");
+        } else {
+            addForce();
+            menu.style.setProperty("display", "none");
         }
     }
 })
+
+let forceList = [];
+
+function addForce() {
+
+    let force = {
+        "magnitude": forceMagnitude.value,
+        "direction": forceDirection.value
+    }
+
+    forceList.push(force);
+
+    for (let i = 0; i <= forceList; i++) {
+        let vector = document.createElement("div");
+        vector.style.setProperty("width", forceList[i].magnitude);
+        vector.style.setProperty("height", "5px");
+        vector.style.setProperty("background-color", "red");
+        vector.setAttribute("arrow");
+        canvas.appendChild(vector);
+    }
+}
