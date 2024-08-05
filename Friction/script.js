7dab62a5dd0bd83369a0f982ca10c3f72b196c53
Abdelrahman Ibrahim <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2024-08-05 14:38:16 +0300

    Add files via upload

--- Friction/script.js
+++ Friction/script.js
@@ -3,12 +3,30 @@ let us_txt = document.getElementById("fsc");
 let uk_txt = document.getElementById("fkc");
 let fapp_txt = document.getElementById("fa");
 g_txt.value = 9.81;
+let d = 50;
 
-let r = 20;
+let canvas = document.getElementById("canvas");
+canvas.width = 700;
+canvas.height = d;
+let ctx = canvas.getContext("2d");
+
+let X_init = (canvas.width - (d/2))/2;
 
 document.addEventListener("click", function (e) {
     let targetElement = e.target;
     if (targetElement.id === "start") {
-        
+        draw();    
     }
 })
+
+function dx() {
+
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.rect(X_init, 0, d, d);
+    ctx.fill();
+}
