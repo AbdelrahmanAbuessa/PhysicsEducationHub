bbfb25f3129d694380ece7222c5e6047b30c9fec
Abdelrahman Ibrahim <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2024-08-07 22:55:50 +0300

    Add files via upload

--- Friction/script.js
+++ Friction/script.js
@@ -19,8 +19,9 @@ let app_friction;
 let type_friction;
 let kinetic_friction;
 let weight;
-let opposing_static_forces;
-let opposing_kinetic_forces;
+let weight_inclined;
+let max_friction_inclined;
+let kinetic_friction_inclined;
 
 let canvas = document.getElementById("canvas");
 canvas.width = 700;
@@ -61,20 +62,33 @@ function start() {
     weight = mass * g;
     max_friction = weight * us;
     kinetic_friction = weight * uk;
-    opposing_static_forces = weight * (Math.sin(theta) + (Math.cos(theta) * us));
-    opposing_kinetic_forces = weight * (Math.sin(theta) + (Math.cos(theta) * uk));
-
+    weight_inclined = weight * Math.sin(theta);
+    max_friction_inclined = max_friction * Math.cos(theta);
+    kinetic_friction_inclined = kinetic_friction * Math.cos(theta);
+    
     if (theta !== 0) {
-        canvas.style.rotate = `-${theta}rad`;
-        pos = (canvas.width - d) / 2;
-        if (fa <= opposing_static_forces) {
-            type_friction = "static";
-            app_inclined_friction = opposing_static_forces;
-            draw(pos);
-        } else if (fa > opposing_static_forces) {
-            type_friction = "kinetic";
-            app_inclined_friction = opposing_kinetic_forces;
-            requestAnimationFrame(updatePositionInclined);
+        if (fa === 0) {
+            if (weight_inclined <= max_friction_inclined) {
+                app_friction = weight_inclined;
+                type_friction = "static";
+                pos = (canvas.width - d) / 2;
+                draw(pos);
+            } else if (weight_inclined > max_friction_inclined) {
+                type_friction = "kinetic";
+                app_friction = kinetic_friction_inclined;
+                pos = (canvas.width - d) / 2;
+                requestAnimationFrame(updatePositionInclinedNoAppliedForce);
+                console.log("this is activated");
+            }
+        } else if (fa > 0) {
+            // check if the force is greater than the sum of the normal friction AND the weight 
+            // if the force is not greater
+            // calculate the difference between the force and the friction AND weight
+            // calculate acceleration using total F = ma
+            // apply acceleration and slide down
+            // if force is greater
+            // calculate acceleration using total F = ma
+            // apply acceleration and push upwards
         }
     } else {
         if (fa <= max_friction) {
@@ -97,7 +111,7 @@ function draw(pos) {
     ctx.beginPath();
     ctx.rect(pos, 0, d, d);
     ctx.fill();
-
+    
     ctx.fillStyle = 'white';
     ctx.font = '20px Arial';
     const textMetrics = ctx.measureText(mass);
@@ -124,17 +138,15 @@ function updatePositionStatic() {
     }
 }
 
-function updatePositionInclined() {
-    pos += dx;
-    dx += (fa - app_inclined_friction) / mass / 10;
+function updatePositionInclinedNoAppliedForce() {
+    pos -= dx;
+    dx += (weight_inclined - app_friction) / mass / 10;
     draw(pos);
     if (pos < canvas.width - d && pos >= 0) {
-        animationFrameId = requestAnimationFrame(updatePositionInclined);
-    } else if (pos >= canvas.width - d) {
-        pos = canvas.width - d;
-        dx = 0;
+        animationFrameId = requestAnimationFrame(updatePositionInclinedNoAppliedForce);
     } else if (pos < 0) {
         pos = 0;
         dx = 0;
     }
+    console.log(dx);
 }
