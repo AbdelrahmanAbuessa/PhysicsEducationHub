37efa3f94e4e2b6a7eb4365c838f489f55f55cd6
Abdelrahman Ibrahim <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2024-08-19 23:15:57 +0300

    Add files via upload

--- FluidPressure-U/script.js
+++ FluidPressure-U/script.js
@@ -3,6 +3,105 @@ canvas.width = 700;
 canvas.height = 450;
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.fillStyle = "black";
-ctx.fillRect(10, 10, 100, 100);
+let fluidDensity_A_txt = document.getElementById("raw1");
+let fluidDensity_B_txt = document.getElementById("raw2");
+let fluidHeight_A_txt = document.getElementById("h1");
+let fluidHeight_B_txt = document.getElementById("h2");
+let gaugePressure_A_txt = document.getElementById("Gp1");
+let gaugePressure_B_txt = document.getElementById("Gp2");
+let AbsPressure_A_txt = document.getElementById("Ap1");
+let AbsPressure_B_txt = document.getElementById("Ap2");
+let atm_txt = document.getElementById("atm");
+let g_txt = document.getElementById("g");
+let gaugePressure_A;
+let gaugePressure_B;
+let AbsPressure_A;
+let AbsPressure_B;
+let fluidHeight_B;
+
+drawTube();
+
+fluidA(100);
+fluidB(300);
+
+function drawTube() {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.fillRect((canvas.width - 200) / 2, 20, 5, 300);
+    ctx.fillRect(((canvas.width - 200) / 2) + 200, 20, 5, 300);
+    ctx.fillRect(((canvas.width - 200) / 2) + 150, 20, 5, 300);
+    ctx.fillRect(((canvas.width - 200) / 2) + 50, 20, 5, 300);
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(352.5, 320, 50, 0, Math.PI, false);
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(352.5, 320, 100, 0, Math.PI, false);
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(352.5, 320, 75, 0, Math.PI, false);
+    ctx.lineWidth = 45;
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+    ctx.closePath();
+    
+    for (let i = 100; i <= canvas.width - 100; i += 25) {
+        ctx.beginPath();
+        ctx.fillStyle = "black";
+        ctx.fillRect(i, 320, 20, 5);
+        ctx.closePath();
+    }
+    
+    ctx.beginPath();
+    
+    ctx.closePath();
+}
+
+function fluidA(height) {
+    ctx.beginPath();
+    ctx.fillStyle = "blue";
+    ctx.fillRect(((canvas.width - 200) / 2) + 5, 20 + (300 - height), 45, height);
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.arc(277.5, 322.5, 15, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.fillStyle = 'white';
+    ctx.font = '20px Arial';
+    const textMetrics = ctx.measureText("A");
+    const textWidth = textMetrics.width;
+    const textHeight = parseInt(ctx.font, 10);
+    const x = 277.5 - textWidth / 2;
+    const y = 322.5 + textHeight / 2 / 1.5;
+    ctx.fillText("A", x, y);
+    ctx.closePath();
+}
+
+function fluidB(height) {
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.fillRect(((canvas.width - 200) / 2) + 155, 20 + (300 - height), 45, height);
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.arc(427.5, 322.5, 15, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.fillStyle = 'white';
+    ctx.font = '20px Arial';
+    const textMetrics = ctx.measureText("B");
+    const textWidth = textMetrics.width;
+    const textHeight = parseInt(ctx.font, 10);
+    const x = 427.5 - textWidth / 2;
+    const y = 322.5 + textHeight / 2 / 1.5;
+    ctx.fillText("B", x, y);
+    ctx.closePath();
+}
