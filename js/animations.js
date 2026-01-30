ddb4044e4abf1dec3bc4529e7ed4e6716ada121a
Abdelrahman Aboueissa <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2026-01-30 20:57:27 +0200

    Project Revamp #1
    Revamping the Project.
    Added new pages and new branches of the project to explore, all responsive and functional. Used Bootstrap instead of vanilla CSS. Overall improved the UI of the main pages of the Project.

---
+++ js/animations.js
@@ -0,0 +1,21 @@
+let items = document.querySelectorAll("section > ul > li");
+items.forEach((item, index) => {
+    item.classList.remove("left-entry", "right-entry");
+    item.classList.add(index % 2 ? "right-entry" : "left-entry");
+});
+
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.15,
+    },
+);
+
+items.forEach((item) => observer.observe(item));
