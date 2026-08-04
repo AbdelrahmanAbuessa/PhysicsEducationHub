f9cb8b91a5d16850907a4d03031480a2a995650a
Abdelrahman Aboueissa <143011605+AbdelrahmanAbuessa@users.noreply.github.com>
2026-08-04 09:22:52 +0300

    Finished resultant logic and fixed overflow issues

--- Resultant/force-logic.js
+++ Resultant/force-logic.js
@@ -31,15 +31,11 @@ let forceList = [];
 let forceListHTML = document.getElementById("force-list");
 
 class Force {
-    #color = assignColor();
+    color = assignColor();
     id = -1;
     constructor(mag, dir) {
         this.mag = mag;
         this.dir = dir;
-    }
-
-    getColor() {
-        return this.#color;
     }
 
     getX() {
@@ -50,6 +46,11 @@ class Force {
         return this.mag * Math.sin((this.dir * Math.PI) / 180);
     }
 
+    update(mag, dir) {
+        this.mag = mag;
+        this.dir = dir;
+    }
+
     drawForce() {
         ctx.save();
         updateCenter();
@@ -58,18 +59,18 @@ class Force {
 
         ctx.lineWidth = 5;
         ctx.globalAlpha = 1;
-        ctx.strokeStyle = this.getColor();
-        ctx.fillStyle = this.getColor();
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
 
         ctx.beginPath();
         ctx.moveTo(0, 0);
-        ctx.lineTo(0, this.mag * scale);
+        ctx.lineTo(0, this.mag * scale * baseUnit);
         ctx.stroke();
         ctx.closePath();
 
         let arrowSize = 20;
         let tipX = 0;
-        let tipY = this.mag * scale + arrowSize / 2;
+        let tipY = this.mag * baseUnit * scale + arrowSize / 2;
 
         ctx.beginPath();
         ctx.moveTo(tipX, tipY);
@@ -83,12 +84,14 @@ class Force {
 }
 
 class Resultant extends Force {
-    #color = "#FF0000";
     id = "resultant";
     constructor(mag, dir) {
         super(mag, dir);
+        this.color = "#FF0000";
     }
 }
+
+let resultant = new Resultant(0, 0);
 
 let magnitudeInput = document.getElementById("mag");
 let directionInput = document.getElementById("dir");
@@ -99,7 +102,7 @@ function forceId() {
         forceList[i].id = i + 1;
         let forceHTML = `<div
                                 class="force d-flex justify-content-around align-items-center p-2 mb-2"
-                                style="background-color: ${forceList[i].getColor()}"
+                                style="background-color: ${forceList[i].color}"
                                 id="${forceList[i].id}"
                             >
                                 <p class="m-0 force-label">F${forceList[i].id}</p>
@@ -123,10 +126,18 @@ function drawAllForces() {
     forceList.forEach((force) => {
         force.drawForce();
     });
+    updateResultant();
+}
+
+function AddForce(mag, dir) {
+    let force = new Force(mag, dir);
+    forceList.push(force);
+    forceId();
+    drawAllForces();
 }
 
 function constructForce() {
-    let mag = magnitudeInput.value * baseUnit;
+    let mag = magnitudeInput.value;
     let dir = directionInput.value * 1.0;
 
     let force = new Force(mag, dir);
@@ -134,8 +145,6 @@ function constructForce() {
 
     forceId();
 }
-
-function addForceToList(force) {}
 
 function add_force() {
     toggle_sim();
@@ -149,11 +158,42 @@ function delete_force(i) {
     drawAllForces();
 }
 
+function updateResultant() {
+    let resultantX = 0;
+    let resultantY = 0;
+
+    forceList.forEach((force) => {
+        resultantX += force.getX();
+        resultantY += force.getY();
+    });
+
+    let resultantMag =
+        Math.floor(
+            Math.sqrt(resultantX * resultantX + resultantY * resultantY) * 10,
+        ) / 10;
+
+    let resultantDir =
+        Math.floor(Math.atan2(resultantY, resultantX) * (180 / Math.PI) * 10) /
+        10;
+
+    resultant.update(resultantMag, resultantDir);
+
+    if (resultantX != 0 || resultantY != 0) {
+        resultant.drawForce();
+    }
+}
+
 function select_force(i) {
     let forceFieldID = document.getElementById("force-prop-id");
     let forceFieldmag = document.getElementById("force-prop-mag");
     let forceFielddir = document.getElementById("force-prop-dir");
-    forceFieldID.innerText = forceList[i].id;
-    forceFieldmag.innerText = forceList[i].mag / baseUnit;
-    forceFielddir.innerText = forceList[i].dir;
+    if (i == -1) {
+        forceFieldID.innerText = resultant.id;
+        forceFieldmag.innerText = resultant.mag;
+        forceFielddir.innerText = resultant.dir;
+    } else {
+        forceFieldID.innerText = `#${forceList[i].id}`;
+        forceFieldmag.innerText = forceList[i].mag;
+        forceFielddir.innerText = forceList[i].dir;
+    }
 }
